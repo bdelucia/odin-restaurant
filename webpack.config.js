a60af40eec245463f3906e4340d8384dee47b8e8
Bobby DeLucia <bdelucia@asu.edu>
2024-12-26 03:48:04 -0700

    webpack init

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Path to your HTML file
+    }),
+    ],
+    devServer: {
+        static: './dist', // Serve from the "dist" folder
+    },
+};
