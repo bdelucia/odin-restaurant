3397a463538d291d3cef31b0773a9e2dccd1029f
Robert DeLucia Jr. <bdelucia@asu.edu>
2024-12-26 18:47:40 -0700

    image again

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import "./styles.css";
+import chickenparm from "./Chicken-Parmesan-1.jpg"
+const image = document.createElement("img");
+image.src = chickenparm;
+   
+document.body.appendChild(image);
 alert('what up playa');
