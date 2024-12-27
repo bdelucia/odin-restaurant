0ee34cd245d25615274ea6975e8cb56940ffab5c
Robert DeLucia Jr. <bdelucia@asu.edu>
2024-12-27 03:04:38 -0700

    createabout

---
+++ src/about.js
@@ -0,0 +1,9 @@
+export function createAbout() {
+    const content = document.getElementById('content');
+    content.textContent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque, architecto quae? 
+                           Quibusdam sapiente reprehenderit, numquam veritatis dolores deserunt, nostrum 
+                           optio odit nulla nobis nam explicabo obcaecati atque ad culpa eius.`
+    content.style.color = 'white';
+    content.style.fontSize = '2rem';
+    content.style.fontWeight = 'bold';
+}
