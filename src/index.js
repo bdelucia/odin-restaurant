a71a0bd78630b0fe5269201182e70b7949435bb5
Robert DeLucia Jr. <bdelucia@asu.edu>
2024-12-27 00:28:29 -0700

    wwer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import "./styles.css";
 import chickenparm from "./Chicken-Parmesan-1.jpg"
+import background from "./background.jpg"
 import { createHeadline } from "./initial-load.js";
-
+import { clearContent } from "./clear-content.js";
 const content = document.getElementById('content');
+document.body.style.backgroundImage = `url(${background})`;
+document.body.style.backgroundSize = 'cover'; 
+document.body.style.backgroundPosition = 'center'; 
+document.body.style.backgroundRepeat = 'no-repeat';
+document.body.style.overflow = 'hidden';
 
 const image = document.createElement("img");
 image.src = chickenparm;
